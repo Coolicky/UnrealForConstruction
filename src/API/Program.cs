cfe32f3ef7714f21eecf078d71f22af0f4ddbc0a
coolicky <piotr.kulicki@gmail.com>
2023-02-07 20:41:10 +0000

    Injected Minio

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,8 +1,12 @@
 using API.Data;
 using Data;
+using Infrastructure.Data;
+using Infrastructure.Services;
+using Infrastructure.Storage;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
+using Minio;
 
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
@@ -24,6 +28,30 @@ builder.Services.AddDbContext<UnrealContext>(options =>
             config.GetConnectionString($"Database:ConnectionStrings:{Provider.SqlServer.Name}")!,
             x => x.MigrationsAssembly(Provider.SqlServer.Assembly));
 });
+builder.Services.AddTransient(typeof(IUnrealRepository<>), typeof(EfCoreRepository<>));
+builder.Services.AddTransient(typeof(IUnrealFileRepository<>), typeof(EfCoreFileRepository<>));
+
+var storageProvider = config.GetValue("Storage:Provider", "S3");
+var settings = new StorageSettings
+{
+    ExpiryTime = config.GetValue<int>("Storage:ExpiryTime"),
+    BucketLocation = config.GetValue<string>("Storage:BucketLocation") ?? "logistics"
+};
+services.AddSingleton(settings);
+if (storageProvider == "S3")
+{
+    var client = new MinioClient()
+        .WithEndpoint(config["Storage:S3:endpoint"])
+        .WithCredentials(config["Storage:S3:accessKey"], config["Storage:S3:secretKey"])
+        .WithSSL(config.GetValue<bool>("Storage:S3:secure"))
+        .Build();
+    services.AddSingleton(client);
+    services.AddScoped(typeof(IUnrealStorageService<>), typeof(S3FileService<>));
+}
+else
+{
+    
+}
 
 services.AddApiVersioning(options =>
 {
