c595518ff432f03f9ffcccb1680966650b3baf04
Piotr Kulicki <piotr.kulicki@gmail.com>
2023-03-01 16:16:18 +0000

    added JWT api auth

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,11 +1,20 @@
 using API.Extensions;
 using Data;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
 var services = builder.Services;
+
+
+services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = "https://auth.pmgroupdigital.com/application/o/construction-logistics/";
+        options.Audience = "ef016c17dbcd830377fe57e99f035f0c1604b3d7";
+    });
+
+services.AddAuthorization();
 
 services.ConfigureDatabaseProvider(config);
 services.ConfigureStorageProvider(config);
@@ -27,8 +36,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapControllers()
+    .RequireAuthorization();
 
 app.Run();
