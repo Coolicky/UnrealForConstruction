a071d16fc6eebb1f4938106d7b1870c3411e79a3
Piotr Kulicki <piotr.kulicki@gmail.com>
2023-03-01 17:19:08 +0000

    nuke created

---
+++ build/Build.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.Execution;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.Docker.DockerTasks;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+class Build : NukeBuild
+{
+    public static int Main () => Execute<Build>(x => x.Compile);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+    
+    readonly AbsolutePath APIProject = RootDirectory / "src" / "Coolicky.ConstructionLogistics.Api";
+
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            
+        });
+
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+        });
+
+}
