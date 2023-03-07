428cbdc9d05bbc1de446731dbe7434f310add042
coolicky <piotr.kulicki@gmail.com>
2023-03-07 12:51:00 +0000

    updated build & actions

--- build/Build.Docker.cs
+++ build/Build.Docker.cs
@@ -1,8 +1,11 @@
+using System;
 using Nuke.Common;
 using Nuke.Common.Git;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.Docker;
 using Nuke.Common.Tools.GitVersion;
+using Polly;
+using Serilog;
 using static Nuke.Common.Tools.Docker.DockerTasks;
 using static Nuke.GitHub.GitHubTasks;
 
@@ -10,7 +13,8 @@ partial class Build
 {
     [GitVersion] readonly GitVersion GitVersion;
     [GitRepository] readonly GitRepository GitRepository;
-    [Secret] [Parameter("Github Personal Access Token", Name = "Access Token")] string GitHubToken;
+    [Secret] 
+    [Parameter("Github Personal Access Token")] string GitHubToken;
 
     [Parameter("The GitHub user account that will be used to push the Docker image to the container registry")]
     readonly string GitHubUsername;
@@ -43,6 +47,20 @@ partial class Build
         .OnlyWhenDynamic(() => GitRepository.IsOnMainOrMasterBranch())
         .Executes(() =>
         {
+            Policy.Handle<Exception>()
+                .WaitAndRetry(5,
+                    retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                    (ex, timeSpan, retryCount, context) =>
+                    {
+                        Log.Warning("Docker login exited with code: {Ex}", ex);
+                        Log.Information("Attempting to login into GitHub Docker image registry. Try #{RetryCount}", retryCount);
+                    })
+                .Execute(() => DockerLogin(settings => settings
+                    .SetServer("https://ghcr.io")
+                    .SetUsername(GitHubUsername)
+                    .SetPassword(GitHubToken)
+                    .DisableProcessLogOutput()));
+            
             var (repositoryOwner, repositoryName) = GetGitHubRepositoryInfo(GitRepository);
 
             var versionImageName =
--- build/Build.Properties.cs
+++
@@ -1,7 +0,0 @@
-using Nuke.Common;
-using Nuke.Common.IO;
-using Nuke.Common.ProjectModel;
-
-partial class Build
-{
-}
